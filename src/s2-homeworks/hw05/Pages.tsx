7dbb36172d8933e7c7565d9253fbb5a1e6415689
Komandr33 <and.kom2017@gmail.com>
2024-04-01 21:20:54 +0300

    Completed routes in Pages.tsx

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Error404 from './pages/Error404'
+import {Navigate, Route, Routes} from 'react-router-dom'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
+import Error404 from './pages/Error404';
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -13,22 +13,22 @@ export const PATH = {
 
 function Pages() {
     return (
-        <div>
-            {/*Routes выбирает первый подходящий роут*/}
-            <Routes>
-                {/*роутинг будут писать студенты*/}
-                {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+      <div>
+        {/*Routes выбирает первый подходящий роут*/}
+        <Routes>
+          {/*<NavLink to={'/'}/>*/}
+          {/*роутинг будут писать студенты*/}
+          {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
+          <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+          {/*роуты для /pre-junior, /junior, /junior-plus*/}
+          <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+          <Route path={PATH.JUNIOR} element={<Junior/>}/>
+          <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
-                {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
-            </Routes>
-        </div>
+          {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
+          <Route path={'/*'} element={<Error404/>}/>
+        </Routes>
+      </div>
     )
 }
 
