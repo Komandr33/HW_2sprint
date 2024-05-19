5fdc5edd8ee74cfda205c30a7169e21bfe786cdd
Komandr33 <and.kom2017@gmail.com>
2024-05-19 14:09:00 +0300

    completed homeWorkReducer.ts in the HW7

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const sortFunc = (a: UserType, b: UserType) => {
+                if (a.name < b.name) {
+                    return -1
+                } else if (a.name > b.name) {
+                    return 1
+                } else return 0
+            }
+            return action.payload === 'up' ? [...state].sort(sortFunc) : [...state].sort(sortFunc).reverse()
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return [...state].filter(u => u.age > 17) // need to fix
         }
         default:
             return state
--- src/s2-homeworks/hw08/bll/tests/homeWorkReducer.test.tsx
+++ src/s2-homeworks/hw08/bll/tests/homeWorkReducer.test.tsx
@@ -8,7 +8,7 @@ beforeEach(() => {
     initialState = [
         { _id: 0, name: 'Кот', age: 3 },
         { _id: 1, name: 'Александр', age: 66 },
-        { _id: 2, name: 'Коля', age: 16 },
+        { _id: 2, name: 'Яна', age: 16 },
         { _id: 3, name: 'Виктор', age: 44 },
         { _id: 4, name: 'Дмитрий', age: 40 },
         { _id: 5, name: 'Ирина', age: 55 },
@@ -24,12 +24,13 @@ test('sort name up', () => {
     expect(newState[0]._id).toBe(1)
 })
 test('sort name down', () => {
+    debugger
     const newState = homeWorkReducer(initialState, {
         type: 'sort',
         payload: 'down',
     })
 
-    expect(newState[0]._id).toBe(0)
+    expect(newState[0]._id).toBe(2)
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {
