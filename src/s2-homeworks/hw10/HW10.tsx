922e22497e4653d11f8c01a7f64180b36a322a94
Komandr33 <and.kom2017@gmail.com>
2024-07-09 23:53:03 +0300

    completed HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -7,19 +7,23 @@ import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
 
 /*
-* 1 - в файле loadingReducer.ts дописать типы и логику
-* 2 - получить isLoading из редакса
-* 3 - дописать функцию setLoading
-* 4 - сделать стили в соответствии с дизайном
+* 1 - в файле loadingReducer.ts дописать типы и логику +++
+* 2 - получить isLoading из редакса +++
+* 3 - дописать функцию setLoading +++
+* 4 - сделать стили в соответствии с дизайном +++
 * */
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector((state: AppStoreType)=> state.loading.isLoading)
+    const dispatch = useDispatch()
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+      dispatch(loadingAC(!isLoading))
+        setTimeout(()=>{
+          dispatch(loadingAC(!isLoading))
+        },1500)
         // setTimeout
     }
 
